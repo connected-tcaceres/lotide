6c1d367313130dfa9a41f8df5cf02092a08c8915
Tyler Caceres <tyler.caceres@gmail.com>
2019-06-25 19:18:02 +0000

    files that have been changed to upload

--- tail.js
+++ tail.js
@@ -1,16 +1,16 @@
 const assertEqual = function(actual,expected) {
   if (actual === expected) {
-    return console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   } else {
-    return console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
 const tail = function(arr = []) {
-  if(arr.length > 1) {
+  if (arr.length > 1) {
     return arr.slice(1);
   } else {
-    return undefined;
+    return [];
   }
 };
 
@@ -19,5 +19,6 @@ console.log(tail([3,2,1]));
 console.log(tail(["yes","no","maybe",5,6,7]));
 console.log(tail());
 console.log(tail([]));
+console.log(tail([2]));
 
 assertEqual(tail([1,2,3]).join(),[2,3].join());
