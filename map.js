7871da586b37494ee5aceca21e6c08c8e4c0f7a4
Tyler Caceres <tyler.caceres@gmail.com>
2019-06-27 19:36:58 +0000

    created and completed map.js

---
+++ map.js
@@ -0,0 +1,33 @@
+const assertArraysEqual = function(arr1,arr2) {
+  if (eqArrays(arr1,arr2)) {
+    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+const eqArrays = function(arr1,arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const words = ["ground", "control", "to", "major", "tom"];
+
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+};
+
+assertArraysEqual(map(['yes','no'],word => word[0]),['y','n']);
+assertArraysEqual(map(['tyler','caceres'],word => word[3]),['e','e']);
