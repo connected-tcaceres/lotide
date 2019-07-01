23acdc75c80b9cde43b62a74f086022f9326de9f
Tyler Caceres <tyler.caceres@gmail.com>
2019-07-01 22:08:10 +0000

    eqarrays and eqobjects working correctly

--- eqArrays.js
+++ eqArrays.js
@@ -1,14 +1,27 @@
 const eqArrays = function(arr1,arr2) {
+  // console.log(arr1, arr2);
   if (arr1.length !== arr2.length) {
     return false;
   }
 
   for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
+    if(arr1[i] !== arr2[i]){
+      if (Array.isArray(arr1[i]) && Array.isArray(arr2[i])) {
+        if (!eqArrays(arr1[i], arr2[i])) {
+          return false;
+        }
+      } else {
+
+        return false;
+      }
     }
   }
+
   return true;
 };
 
 module.exports = eqArrays;
+
+console.log(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true); // => true
+console.log(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false); // => false
+console.log(eqArrays([[2, 3], [4]], [[2, 3], 4]), false); // => false
--- eqObjects.js
+++ eqObjects.js
@@ -4,20 +4,22 @@ const eqObjects = function(object1, object2) {
   let keysObj1 = Object.keys(object1).sort();
   let keysObj2 = Object.keys(object2).sort();
 
-  if(keysObj1.length !== keysObj2.length){
-    return false;
-  } else {
-    for(const key of keysObj1){
+  if(keysObj1.length !== keysObj2.length) return false;
+    
+  for(const key of keysObj1){
       if(Array.isArray(object1[key]) && Array.isArray(object2[key])){
         if(!eqArrays(object1[key],object2[key])){
           return false;
         }
       }
-      else if(object1[key] !== object2[key]){
+      if(typeof object1[key] === 'object' && typeof object2[key] === 'object'){
+        return eqObjects(object1[key],object2[key]);
+      }
+
+      if(object1[key] !== object2[key]){
         return false;
       }
     }
-  }
   return true;
 };
 
